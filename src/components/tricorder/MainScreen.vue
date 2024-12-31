56cfe03488f0df9dc1534b2414240a5fcfc3635d
nuForge <nuforge@gmail.com>
2024-12-31 00:31:55 -0500

    added sparklines

--- src/components/tricorder/MainScreen.vue
+++ src/components/tricorder/MainScreen.vue
@@ -1,12 +1,12 @@
-<script setup lang="ts">
-import DataScreen from '@/components/tricorder/screens/DataScreen.vue';
-
-</script>
-
 <template>
   <v-card border="text md" class="bg-background rounded" variant="outlined" min-height="256">
     <v-card-text>
-      <DataScreen />
+      <DataCard />
     </v-card-text>
   </v-card>
 </template>
+
+<script setup lang="ts">
+import DataCard from '@/components/tricorder/screens/DataCard.vue';
+
+</script>
---
+++ src/components/tricorder/screens/DataCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-sparkline :gradient="gradient" :model-value="value" height="100%" padding="24" :color="color"
+    stroke-linecap="round" smooth>
+  </v-sparkline>
+</template>
+
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useScannerStore } from '@/stores/scanner';
+const scanner = useScannerStore()
+
+const color = computed(() => `rgb(${scanner.device.getValue('delta')}, ${scanner.device.getValue('gamma')}, ${scanner.device.getValue('beta')})`)
+
+
+const value = computed(() => [
+  scanner.device.getValue('alpha') as number,
+  scanner.device.getValue('beta') as number,
+  scanner.device.getValue('gamma') as number,
+  scanner.device.getValue('delta') as number,
+])
+
+const gradient = computed(() => ['white', 'red', 'green', 'blue'],)
+
+
+
+</script>
